b1750cc50f0fd4995f366c76fa147f1877f75644
Átila Camurça <camurca.home@gmail.com>
2019-05-29 10:54:15 -0300

    Updates

---
+++ assets/js/page--node-modules--gridsome--app--pages--404-vue.0322d330.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{494:function(n,t,e){"use strict";e.r(t);var s=e(2),u=Object(s.a)({},function(){var n=this.$createElement;return(this._self._c||n)("h1",[this._v("404 - not found")])},[],!1,null,null,null);t.default=u.exports}}]);
--- assets/js/page--node-modules--gridsome--app--pages--404-vue.b1d9cfd2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{495:function(n,t,e){"use strict";e.r(t);var s=e(2),u=Object(s.a)({},function(){var n=this.$createElement;return(this._self._c||n)("h1",[this._v("404 - not found")])},[],!1,null,null,null);t.default=u.exports}}]);
